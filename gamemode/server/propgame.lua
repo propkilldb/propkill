f95509bfce1ee907a621a4029f0dea0c8764ba86
Iced Coffee <me@icedd.coffee>
2025-03-23 19:12:16 +1100

    propgame update

--- gamemode/server/propgame.lua
+++ gamemode/server/propgame.lua
@@ -1,25 +1,67 @@
 
 local event = newEvent("propgame")
-local proplist = {
-	"models/props_canal/canal_bars004.mdl",
-	"models/props/cs_militia/refrigerator01.mdl",
-	"models/props/de_port/cargo_container01.mdl",
-	"models/props_junk/bicycle01a.mdl",
-	"models/props/cs_militia/bathroomwallhole01_tile.mdl",
-	"models/props_junk/wheebarrow01a.mdl",
-	"models/props_c17/statue_horse.mdl",
-	"models/props_c17/playground_carousel01.mdl",
-	"models/props/cs_militia/skylight_glass.mdl",
-	"models/props_lab/cactus.mdl",
-	"models/props_trainstation/light_256ceilingmounted001a.mdl",
-	"models/props/de_train/utility_truck.mdl",
-	"models/props_junk/gascan001a.mdl",
-	"models/mechanics/wheels/wheel_spike_48.mdl",
-	"models/props/de_tides/gate_large.mdl",
-	"models/props_junk/watermelon01.mdl",
+local proplists = {
+	{
+		"models/props_canal/canal_bars004.mdl",
+		"models/props/cs_militia/refrigerator01.mdl",
+		"models/props/de_port/cargo_container01.mdl",
+		"models/props_junk/bicycle01a.mdl",
+		"models/props/cs_militia/bathroomwallhole01_tile.mdl",
+		"models/props_junk/wheebarrow01a.mdl",
+		"models/props_c17/statue_horse.mdl",
+		"models/props_c17/playground_carousel01.mdl",
+		"models/props/cs_militia/skylight_glass.mdl",
+		"models/props_lab/cactus.mdl",
+		"models/props_trainstation/light_256ceilingmounted001a.mdl",
+		"models/props/de_train/utility_truck.mdl",
+		"models/props_junk/gascan001a.mdl",
+		"models/mechanics/wheels/wheel_spike_48.mdl",
+		"models/props/de_tides/gate_large.mdl",
+		"models/props_junk/watermelon01.mdl",
+	},
+	{
+		"models/props/cs_militia/stove01.mdl",
+		"models/props_phx/construct/concrete_pipe01.mdl",
+		"models/props/de_tides/vending_cart.mdl",
+		"models/props_junk/watermelon01.mdl",
+		"models/props_debris/walldestroyed01a.mdl",
+		"models/props_phx/construct/metal_plate4x4.mdl",
+		"models/props/de_inferno/chimney01.mdl",
+		"models/props_phx/misc/soccerball.mdl",
+		"models/props/de_train/lockers_long.mdl",
+		"models/props_phx/facepunch_logo.mdl",
+		"models/props_vehicles/car002b.mdl",
+		"models/props_phx/mk-82.mdl",
+		"models/weapons/w_crowbar.mdl",
+		"models/props_citizen_tech/guillotine001a_base01.mdl",
+		"models/props/CS_militia/dryer.mdl",
+		"models/props/de_inferno/goldfish.mdl",
+	},
+	{
+		"models/props_vehicles/apc_tire001.mdl",
+		"models/props/cs_italy/it_wndc2.mdl",
+		"models/props/cs_assault/moneypallet02a.mdl",
+		"models/maxofs2d/logo_gmod_b.mdl",
+		"models/props/de_tides/vending_turtle.mdl",
+		"models/props_phx/construct/wood/wood_angle360.mdl",
+		"models/props_c17/gaspipes006a.mdl",
+		"models/props_c17/furniturearmchair001a.mdl",
+		"models/props_combine/combineinnerwall001c.mdl",
+		"models/props_canal/canal_cap001.mdl",
+		"models/props_debris/wood_board06a.mdl",
+		"models/props/cs_militia/van.mdl",
+		"models/props_phx/games/chess/white_rook.mdl",
+		"models/props_trainstation/payphone001a.mdl",
+		"models/props_wasteland/prison_celldoor001b.mdl",
+		"models/items/item_item_crate.mdl",
+		"models/props_phx/misc/potato_launcher_explosive.mdl",
+		"models/props/cs_assault/barrelwarning.mdl",
+		"models/xqm/jetbody3.mdl",
+		"models/props/de_tides/menu_stand_p05.mdl",
+	}
 }
 
-event:Hook("PlayerChangedTeam", "battlers cant spectate", function(ply, oldteam, newteam)
+event:Hook("PlayerChangedTeam", "let people not play", function(ply, oldteam, newteam)
 	if newteam == TEAM_DEATHMATCH then
 		ply.battling = true
 		ply.level = 1
@@ -29,25 +71,55 @@ event:Hook("PlayerChangedTeam", "battlers cant spectate", function(ply, oldteam,
 end)
 
 event:Hook("PlayerDeath", "who killed who", function(ply, inflictor, attacker)
-	attacker.level = (attacker.level or 1) + 1
+	if not IsValid(attacker) or not attacker:IsPlayer() then return end
+	attacker.level = attacker.level or 1
+
+	// suicide
+	if ply == attacker then
+		attacker.level = math.max(attacker.level - 1, 1)
+		return
+	end
+
+	// force them to get a kill with the final prop		
+	if attacker.level == #event.proplist and IsValid(inflictor) and inflictor:GetModel() != event.proplist[#event.proplist] then
+		return
+	end
+
+	attacker.level = attacker.level + 1
 	
-	if attacker.level == #proplist then
+	if attacker.level == #event.proplist then
 		ChatMsg({
 			Color(0,120,255), attacker:Nick(),
 			Color(255,255,255), " is on last level!!",
 		})
+
+		attacker:CleanUp()
 	end
 
-	if attacker.level > #proplist then
+	// clean up props from previous levels 1 second after they level up
+	timer.Create("propgame_cleanup_" .. attacker:SteamID(), 1, 1, function()
+		if not IsValid(attacker) then return end
+		local currentmodel = event.proplist[attacker.level]
+
+		for k, ent in ipairs(attacker:GetProps()) do
+			if not IsValid(ent) then continue end
+			if ent:GetModel() != currentmodel then
+				ent:Remove()
+			end
+		end
+	end)
+
+	if attacker.level > #event.proplist then
 		event:End(attacker)
 	end
 end)
 
 event:Hook("PlayerInitialSpawn", "add late joiners to the battle", function(ply)
 	ply.battling = true
+	ply.level = 1
 end)
 
-event:StartFunc(function()
+event:StartFunc(function(proplist)
 	if #player.GetAll() < 2 then
 		return false, "not enough players"
 	end
@@ -59,12 +131,16 @@ event:StartFunc(function()
 		ply.level = 1
 		ply:CleanUp()
 		ply:Freeze(true)
+		ply:SetFrags(0)
+		ply:SetDeaths(0)
 	end
+
+	event.proplist = proplist
 
 	-- kinda hacky, but it works i guess
 	event.CCSpawn = CCSpawn
 	CCSpawn = function(ply, cmd, args, str)
-		args[1] = proplist[ply.level or 1] or ""
+		args[1] = event.proplist[ply.level or 1] or ""
 		return event.CCSpawn(ply, cmd, args, str)
 	end
 	
@@ -74,18 +150,14 @@ event:StartFunc(function()
 		end
 
 		ChatMsg({
-			//Color(255,255,255), "[",
 			Color(0,120,255), "PropGame",
-			//Color(255,255,255), "]",
 			Color(255,255,255), " STARTED!",
 		})
 	end)
 
 	ChatMsg({
-		//Color(255,255,255), "[",
 		Color(0,120,255), "PropGame",
-		//Color(255,255,255), "]",
-		Color(255,255,255), " event starting...",
+		Color(255,255,255), " event starting with " .. tostring(#event.proplist) .. " levels",
 	})
 
 	ResetKillstreak()
@@ -108,9 +180,9 @@ event:EndFunc(function(winner)
 		})
 	end
 
-	timer.Simple(2, function()
-		CCSpawn = event.CCSpawn
+	CCSpawn = event.CCSpawn
 
+	timer.Simple(2, function()
 		for k,v in next, player.GetAll() do
 			if v:Team() != TEAM_DEATHMATCH then continue end
 			v:CleanUp()
@@ -130,7 +202,14 @@ concommand.Add("propgame", function(ply, cmd, args, str)
 		return
 	end
 
-	local success, err = event:Start()
+	local proplist = proplists[1]
+
+	local listnum = tonumber(args[1])
+	if isnumber(listnum) and proplists[listnum] then
+		proplist = proplists[listnum]
+	end
+
+	local success, err = event:Start(proplist)
 	if not success then
 		ply:PrintMessage(HUD_PRINTCONSOLE, tostring(err))
 	end
