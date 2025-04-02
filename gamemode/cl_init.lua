9288de2140ee5bda4d911f474040e813d6e28dd7
Iced Coffee <me@icedd.coffee>
2025-04-02 23:03:06 +1100

    oops

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -5,8 +5,10 @@ include("client/duels.lua")
 include("client/hud.lua")
 include("client/hax.lua")
 include("client/base.lua")
-include("shared/onesurf.lua")
 include("shared/entity.lua")
 
 // derma
 include("client/scoreboard/frame.lua")
+
+// modifiers
+include("modifiers/onesurf.lua")
