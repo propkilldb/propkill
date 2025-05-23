d567031a181f2fd2b9dece5856483c1421764ea8
Iced Coffee <me@icedd.coffee>
2025-05-23 21:48:02 +1000

    tell users to switch branch to see bracket

--- gamemode/client/scoreboard/bracket.lua
+++ gamemode/client/scoreboard/bracket.lua
@@ -3,7 +3,16 @@ PANEL.Base = "DHTML"
 local challonge_id = CreateConVar("pk_challonge_id", "", {FCVAR_REPLICATED, FCVAR_ARCHIVE})
 
 function PANEL:Init()
-	self:UpdateChallongeID()
+	if BRANCH == "unknown" then -- unknown = default branch
+		self:SetHTML([[
+			<br>
+			<center style="color: #fff">
+				<h2>Please change your Garry's Mod to the x86-64 beta branch to see the bracket</h2>
+				<img src="https://images.steamusercontent.com/ugc/1898857260618639534/16524E2140288CD70902A5D845289D29C1450B94/">
+			</center>
+		]])
+		return
+	end
 end
 
 function PANEL:UpdateChallongeID()
@@ -18,6 +27,8 @@ function PANEL:UpdateChallongeID()
 end
 
 function PANEL:RefreshData()
+	if BRANCH == "unknown" then return end
+
 	if challonge_id:GetString() != self.bracketid then
 		self:UpdateChallongeID()
 	end
